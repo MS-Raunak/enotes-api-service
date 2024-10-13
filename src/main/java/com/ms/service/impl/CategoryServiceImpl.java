a222d67a9d7d6f4631d5e0dd9895ab7f6a111bdd
Raunak Kumar <raunakkr.saha@gmail.com>
2024-10-14 01:03:12 +0530

    dto-mapper implementation in category features

--- src/main/java/com/ms/service/impl/CategoryServiceImpl.java
+++ src/main/java/com/ms/service/impl/CategoryServiceImpl.java
@@ -3,10 +3,13 @@ package com.ms.service.impl;
 import java.util.Date;
 import java.util.List;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.util.ObjectUtils;
 
+import com.ms.dto.CategoryDto;
+import com.ms.dto.CategoryResponse;
 import com.ms.entity.Category;
 import com.ms.repository.CategoryRepository;
 import com.ms.service.CategoryService;
@@ -15,12 +18,17 @@ import com.ms.service.CategoryService;
 public class CategoryServiceImpl implements CategoryService{
 	@Autowired
 	CategoryRepository categoryRepo;
+	@Autowired
+	ModelMapper mapper;
+	
 	@Override
-	public Boolean saveCategory(Category category) {
+	public Boolean saveCategory(CategoryDto categoryDto) {
+		Category category = mapper.map(categoryDto, Category.class);
 		category.setIsDeleted(false);
 		category.setCreatedBy(1);
 		category.setCreatedOn(new Date());
 		Category savedCategory = categoryRepo.save(category);
+		
 		if (ObjectUtils.isEmpty(savedCategory)) {
 			return false;
 		}
@@ -28,9 +36,20 @@ public class CategoryServiceImpl implements CategoryService{
 	}
 
 	@Override
-	public List<Category> getAllCategory() {
+	public List<CategoryDto> getAllCategory() {
 		List<Category> categories = categoryRepo.findAll();
-		return categories;
+		List<CategoryDto> categoryDtoList = categories.stream().map(category -> mapper.map(category, CategoryDto.class)).toList();
+		
+		return categoryDtoList;
+	}
+	
+	
+	@Override
+	public List<CategoryResponse> getActiveCategory() {
+		List<Category> categories = categoryRepo.findByIsActiveTrue();
+		List<CategoryResponse> categoryList = categories.stream().map(category -> mapper.map(category, CategoryResponse.class)).toList();
+		
+		return categoryList;
 	}
 
 }
